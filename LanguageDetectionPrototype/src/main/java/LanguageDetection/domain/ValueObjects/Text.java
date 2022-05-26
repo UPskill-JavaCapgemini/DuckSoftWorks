627da47e9e27f8611037777746664b348f26acec
Thales Lemos <thalesflemos@gmail.com>
2022-05-26 20:50:29 +0100

    altered Value Object Text from domaint

--- LanguageDetectionPrototype/src/main/java/LanguageDetection/domain/ValueObjects/Text.java
+++ LanguageDetectionPrototype/src/main/java/LanguageDetection/domain/ValueObjects/Text.java
@@ -1,8 +1,16 @@
 package LanguageDetection.domain.ValueObjects;
 
 import LanguageDetection.domain.shared.ValueObject;
+import lombok.Getter;
 
 public class Text implements ValueObject {
+    @Getter
+    private final String text;
 
-    private String language;
+    public Text(String text) {
+        if (text == null || text.isEmpty())
+            throw new IllegalArgumentException("The text must have at least one character!");
+        this.text = text;
+    }
+
 }
