e4fe86f9e81160f517569ca2e57b756d844090b2
DanielVMachado <danielmachado96@hotmail.com>
2022-05-26 21:34:52 +0100

    Update TaskDTO class

--- LanguageDetectionPrototype/src/main/java/LanguageDetection/application/dtos/TaskDTO.java
+++ LanguageDetectionPrototype/src/main/java/LanguageDetection/application/dtos/TaskDTO.java
@@ -3,14 +3,21 @@ package LanguageDetection.application.dtos;
 import lombok.*;
 import org.springframework.hateoas.RepresentationModel;
 
-@AllArgsConstructor
+import java.util.Date;
+
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = false)
 @ToString
 public class TaskDTO extends RepresentationModel<TaskDTO> {
-    @Getter
-    Long id;
 
     @Getter
-    String name;
+    Date date;
+
+    @Getter
+    String inputText;
+
+    public TaskDTO(String inputText, Date date) {
+        this.inputText=inputText;
+        this.date = date;
+    }
 }
