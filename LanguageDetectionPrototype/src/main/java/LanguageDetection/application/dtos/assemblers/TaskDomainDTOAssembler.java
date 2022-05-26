11196e4ae6231d2848ea7dec0312c25c43e1f713
DanielVMachado <danielmachado96@hotmail.com>
2022-05-26 21:40:46 +0100

    Update TaskDomainDTOAssembler class

--- LanguageDetectionPrototype/src/main/java/LanguageDetection/application/dtos/assemblers/TaskDomainDTOAssembler.java
+++ LanguageDetectionPrototype/src/main/java/LanguageDetection/application/dtos/assemblers/TaskDomainDTOAssembler.java
@@ -3,13 +3,15 @@ package LanguageDetection.application.dtos.assemblers;
 import LanguageDetection.application.dtos.TaskDTO;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
 public class TaskDomainDTOAssembler {
 
     private TaskDomainDTOAssembler() {
     }
 
-    public TaskDTO toDTO (Long id, String name ) {
-        return new TaskDTO(id, name);
+    public TaskDTO toDTO(String language, Date date) {
+        return new TaskDTO(language, date);
     }
 }
