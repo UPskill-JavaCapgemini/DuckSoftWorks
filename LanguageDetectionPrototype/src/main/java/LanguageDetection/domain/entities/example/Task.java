df0446d94c96378bd467233e5455154f52ab24fa
Joao Figueiredo <joaomiguelf@icloud.com>
2022-05-27 09:59:04 +0100

    Deleted Language Interface, Created Task Class

--- LanguageDetectionPrototype/src/main/java/LanguageDetection/domain/entities/example/Task.java
+++ LanguageDetectionPrototype/src/main/java/LanguageDetection/domain/entities/example/Task.java
@@ -1,9 +1,32 @@
 package LanguageDetection.domain.entities.example;
 
+import LanguageDetection.domain.ValueObjects.Language;
+import LanguageDetection.domain.ValueObjects.Text;
 import LanguageDetection.domain.shared.AggregateRoot;
+import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 
-public class Task implements AggregateRoot {
+import java.util.Date;
+@Getter
+public class Task implements AggregateRoot <Date>{
+    Date date;
+    Text text;
+    Language lang;
+
+    public Task (Text txt, Language lg){
+        this.date = new Date(System.currentTimeMillis());
+        this.text = txt;
+        this.lang = lg;
+    }
+
+
+
+
+
+
+
+
+
     @Override
     public boolean sameAs(Object other) {
         return false;
