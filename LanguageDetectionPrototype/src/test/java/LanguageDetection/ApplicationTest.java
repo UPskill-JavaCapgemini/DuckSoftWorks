3dca96d4d7f90dd2e33a0d6ec33ae1c4a3c6add8
Thales Lemos <thalesflemos@gmail.com>
2022-05-28 22:44:07 +0100

    created class TaskControllerTest and ApplicationTest and added a few tests

---
+++ LanguageDetectionPrototype/src/test/java/LanguageDetection/ApplicationTest.java
@@ -0,0 +1,13 @@
+package LanguageDetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- LanguageDetectionPrototype/src/test/java/TaskControllerTest.java
+++
@@ -1,8 +0,0 @@
-import org.testng.annotations.Test;
-
-public class TaskControllerTest {
-
-    @Test
-    public void controller() {
-    }
-}
