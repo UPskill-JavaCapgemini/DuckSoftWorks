2f18393bd0ac30d6588eb5d6f2b6c3152e72cc31
Inês Clavel <inesclavel@gmail.com>
2022-05-29 15:21:02 +0100

    Update AnalyzerService analyze test

--- LanguageDetectionPrototype/src/test/java/LanguageDetection/services/AnalyzerServiceTest.java
+++ LanguageDetectionPrototype/src/test/java/LanguageDetection/services/AnalyzerServiceTest.java
@@ -9,13 +9,14 @@ import java.io.IOException;
 
 public class AnalyzerServiceTest {
 
-/*
     @Test
    void analyze() throws ParseException, IOException {
+
+        AnalyzerService service = new AnalyzerService();
         String phrase = "there was once upon a time a little girl that is annoyed by having to do a lot of unit tests.";
-        Assert.assertEquals(AnalyzerService.analyze(phrase), "English");
+        String result = service.analyze(phrase);
+        Assert.assertEquals(result, "ENGLISH");
         }
-*/
 
 
 }
