2db14c38a3aacc872074997af4c47c968f3423e0
DanielLimaUpskill <daniel.lima-t15@upskill.pt>
2022-05-29 13:18:16 +0100

    Added javadoc for TaskService

--- LanguageDetectionPrototype/src/main/java/LanguageDetection/application/services/TaskService.java
+++ LanguageDetectionPrototype/src/main/java/LanguageDetection/application/services/TaskService.java
@@ -15,21 +15,42 @@ import java.io.IOException;
 import java.util.*;
 
 
-
+/**
+ * Represents the task service responsible for creating a task.
+ * @author DuckSoftWorks
+ */
 @Service
 public class TaskService {
 
     @Autowired
+    /**
+     * The taskFactory.
+     */
     TaskFactory taskFactory;
 
-    @Autowired
-    TaskDomainDTOAssembler taskDomainDTOAssembler;
 
     @Autowired
+    /**
+     * The domain DTO assembler for a task.
+     */
+    TaskDomainDTOAssembler taskDomainDTOAssembler;
+
+
+    @Autowired
+    /**
+     * The Analyzer service.
+     */
     AnalyzerService analyzerService;
 
 
-
+    /**
+     * Creates a new task with a NewTaskInfoDTO received by parameter.
+     * Cleans up the input text by calling cleanUpInputText() and
+     * analyzes it with the service analyzer.
+     *
+     * @param string the string containing the text within NewTaskInfoDTO.
+     * @return  TaskDTO assembled by taskDomainDTOAssembler.
+     */
     public TaskDTO createTask(NewTaskInfoDTO string) throws ParseException, IOException {
         String cleanedUp = cleanUpInputText(string.getText());
         String language = analyzerService.analyze(cleanedUp);
@@ -37,7 +58,13 @@ public class TaskService {
         return taskDomainDTOAssembler.toDTO(task);
     }
 
-
+    /**
+     * Cleans up the string received via input.
+     * Strips the string of multiple whitespaces through the use of a regex.
+     *
+     * @param text the string that is cleaned up with the regex.
+     * @return  the cleaned up text.    
+     */
     private static String cleanUpInputText(String text) {
         return text.trim().toLowerCase(Locale.ROOT)
                 .replaceAll("[^a-zA-Z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u024F]", " ")
