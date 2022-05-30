541fb5dfd9d7ea5b1ba262a3990f10e66de4c60a
Joao Figueiredo <joaomiguelf@icloud.com>
2022-05-30 09:46:06 +0100

    Created class DictionaryServiceTest and tests

--- LanguageDetectionPrototype/src/test/java/LanguageDetection/application/services/DictionaryServiceTest.java
+++ LanguageDetectionPrototype/src/test/java/LanguageDetection/application/services/DictionaryServiceTest.java
@@ -1,7 +1,25 @@
 package LanguageDetection.application.services;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
 
 public class DictionaryServiceTest {
+
+    @Test
+    public void shouldReturnSingletonInstanceOfDictionaryServiceNotNull() throws  IOException {
+        Assert.assertNotNull(DictionaryService.getInstance());
+    }
+
+    @Test
+    public void shouldReturnTheSameInstance() throws  IOException {
+        DictionaryService dictionary1 = DictionaryService.getInstance();
+        DictionaryService dictionary2 = DictionaryService.getInstance();
+
+        Assert.assertEquals(dictionary1, dictionary2);
+    }
+
 
 }
