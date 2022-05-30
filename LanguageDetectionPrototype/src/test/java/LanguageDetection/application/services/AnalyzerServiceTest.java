38dbfb4688e860491b00fd3159dd35d67b64a9bd
Inês Clavel <inesclavel@gmail.com>
2022-05-30 09:24:34 +0100

    AnalyserServiceTest update to add insuccess test and test with more than one language

--- LanguageDetectionPrototype/src/test/java/LanguageDetection/application/services/AnalyzerServiceTest.java
+++ LanguageDetectionPrototype/src/test/java/LanguageDetection/application/services/AnalyzerServiceTest.java
@@ -36,4 +36,22 @@ public class AnalyzerServiceTest {
         Assert.assertEquals(result, "SPANISH");
     }
 
+    @Test
+    void analyzeWithEnglishAndSpanishTest() throws ParseException, IOException {
+
+        AnalyzerService service = new AnalyzerService();
+        String phrase = "There was once upon a time a little girl that is annoyed by having to do a lot of unit tests. Esa chica no soy yo.";
+        String result = service.analyze(phrase);
+        Assert.assertEquals(result, "ENGLISH");
+    }
+
+    @Test
+    void analyzeWithNotPortugueseTest() throws ParseException, IOException {
+
+        AnalyzerService service = new AnalyzerService();
+        String phrase = "There was once upon a time a little girl that is annoyed by having to do a lot of unit tests. Esa chica no soy yo.";
+        String result = service.analyze(phrase);
+        Assert.assertNotEquals(result, "PRTUGUESE");
+    }
+
 }
