11b3c3f7918e08b7e5b64531feb731c9abe45c9a
Inês Clavel <inesclavel@gmail.com>
2022-06-02 11:41:31 +0100

    ITaskFactory, TaskFactory and TaskJpa classes updated

--- LanguageDetectionPrototype/src/main/java/LanguageDetection/infrastructure/datamodel/TaskJpa.java
+++ LanguageDetectionPrototype/src/main/java/LanguageDetection/infrastructure/datamodel/TaskJpa.java
@@ -16,28 +16,18 @@ import java.util.Date;
 @Table(name="task")
 public class TaskJpa {
 
-	// todo: if it's a value object use: @EmbeddedId
-	// todo: the class must be @Embeddable and uses the value object directly from the domain
-	// todo: if it's a generated value, it must implement the EntityId and Serializable interfaces
-
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Getter
 	private Date date;
-
 	@Getter
 	private Task.Category category;
-
 	@Getter
 	private Task.Language language;
-
 	@Getter
 	private TimeOut timeOut;
-
 	@Getter
 	private URL url;
-
 	@Getter
 	private Task.CurrentStatus currentStatus;
 
