3a2bdee795762d3c2a67bd934c500c6eb9ceb233
DanielLimaUpskill <daniel.lima-t15@upskill.pt>
2022-06-02 12:35:39 +0100

    Added BlackListDTO

---
+++ LanguageDetectionPrototype/src/main/java/LanguageDetection/application/dtos/BlackListDTO.java
@@ -0,0 +1,21 @@
+package LanguageDetection.application.dtos;
+
+import LanguageDetection.domain.ValueObjects.InputUrl;
+import LanguageDetection.domain.entities.BlackList;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class BlackListDTO {
+
+    @Getter
+    InputUrl url;
+
+    public BlackListDTO(BlackList blackListItem) {
+        this.url = blackListItem.identity();
+    }
+}
