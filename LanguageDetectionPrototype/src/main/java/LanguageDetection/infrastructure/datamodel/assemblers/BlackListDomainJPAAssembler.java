9420be64219195acf1f7980bd3f6e148ef801234
DanielLimaUpskill <daniel.lima-t15@upskill.pt>
2022-06-02 15:09:13 +0100

    Added BlackListDomainJPAAssembler

--- LanguageDetectionPrototype/src/main/java/LanguageDetection/infrastructure/datamodel/assemblers/BlackListDomainJPAAssembler.java
+++ LanguageDetectionPrototype/src/main/java/LanguageDetection/infrastructure/datamodel/assemblers/BlackListDomainJPAAssembler.java
@@ -2,12 +2,9 @@ package LanguageDetection.infrastructure.datamodel.assemblers;
 
 import LanguageDetection.domain.ValueObjects.InputUrl;
 import LanguageDetection.domain.entities.BlackList;
-import LanguageDetection.domain.entities.Task;
 import LanguageDetection.infrastructure.datamodel.BlackListJPA;
-import LanguageDetection.infrastructure.datamodel.TaskJpa;
 import org.springframework.stereotype.Service;
 
-import javax.naming.MalformedLinkException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -15,8 +12,8 @@ import java.net.URL;
 public class BlackListDomainJPAAssembler {
 
     public BlackListJPA toData(BlackList blackList) {
-         InputUrl inputUrl = blackList.getUrl();
-         String urlPath = inputUrl.getUrl();
+        InputUrl inputUrl = blackList.getUrl();
+        String urlPath = inputUrl.getUrl();
         return new BlackListJPA(urlPath);
     }
 
