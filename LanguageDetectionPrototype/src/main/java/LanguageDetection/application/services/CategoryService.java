c24da13e3e23cccc983700ff8d7335354765a1c9
DanielVMachado <danielmachado96@hotmail.com>
2022-06-04 14:43:12 +0100

    Created CategoryService class with createCategory method
    Still needs to be updated since dependencies are still not created

---
+++ LanguageDetectionPrototype/src/main/java/LanguageDetection/application/services/CategoryService.java
@@ -0,0 +1,29 @@
+package LanguageDetection.application.services;
+
+import LanguageDetection.application.DTO.CategoryDTO;
+import LanguageDetection.application.DTO.DTOAssemblers.CategoryDomainDTOAssembler;
+import LanguageDetection.application.DTO.NewCategoryInfoDTO;
+import LanguageDetection.domain.factories.ICategoryFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    ICategoryFactory categoryFactory;
+
+    @Autowired
+    CategoryDomainDTOAssembler dtoAssembler;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+
+    public CategoryDTO createCategory(NewCategoryInfoDTO infoDTO){
+        Category category = categoryFactory.createCategory(infoDTO.getCategory());
+        Category categoryRepo = categoryRepository.save(category);
+        return dtoAssembler.toDTO(categoryRepo);
+    }
+
+}
