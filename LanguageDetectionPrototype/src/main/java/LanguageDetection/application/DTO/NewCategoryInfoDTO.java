825ceb8f439b22b391be0e37cfecda75a40d62da
DanielVMachado <danielmachado96@hotmail.com>
2022-06-04 14:22:43 +0100

    Added NewCategoryInfo DTO
    DTO that will be received by RequestParameter tag on CategoryController

---
+++ LanguageDetectionPrototype/src/main/java/LanguageDetection/application/DTO/NewCategoryInfoDTO.java
@@ -0,0 +1,15 @@
+package LanguageDetection.application.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class NewCategoryInfoDTO {
+
+    @Getter
+    String category;
+}
