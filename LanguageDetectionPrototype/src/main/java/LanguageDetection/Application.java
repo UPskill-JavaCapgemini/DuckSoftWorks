6d942d3ca131786696d8b99860cc24d3ece6c644
Inês Clavel <inesclavel@gmail.com>
2022-06-07 18:51:52 +0100

    item base for the blacklist created

--- LanguageDetectionPrototype/src/main/java/LanguageDetection/Application.java
+++ LanguageDetectionPrototype/src/main/java/LanguageDetection/Application.java
@@ -1,14 +1,23 @@
 package LanguageDetection;
 
 
+import LanguageDetection.domain.entities.BlackListItem;
+import LanguageDetection.infrastructure.repositories.BlackListRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.net.MalformedURLException;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
         SpringApplication.run(Application.class);
+
+        BlackListItem bl1 = new BlackListItem("http://127.0.0.1.");
+
+        bl1.save()
+
     }
 
 
