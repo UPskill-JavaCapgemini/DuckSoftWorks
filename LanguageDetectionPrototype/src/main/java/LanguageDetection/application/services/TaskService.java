40af6a8ead42c5e4fd3bfe6e6b4fd1131874a05d
Thales Lemos <thalesflemos@gmail.com>
2022-06-07 21:35:30 +0100

    corrected conflict with method createTask

--- LanguageDetectionPrototype/src/main/java/LanguageDetection/application/services/TaskService.java
+++ LanguageDetectionPrototype/src/main/java/LanguageDetection/application/services/TaskService.java
@@ -2,6 +2,7 @@
 package LanguageDetection.application.services;
 
 
+import LanguageDetection.application.DTO.NewBlackListInfoDTO;
 import LanguageDetection.application.DTO.NewTaskInfoDTO;
 import LanguageDetection.application.DTO.TaskDTO;
 import LanguageDetection.application.DTO.DTOAssemblers.TaskDomainDTOAssembler;
@@ -15,7 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-
+import java.util.Optional;
 
 
 /**
@@ -31,8 +32,8 @@ public class TaskService {
     @Autowired
 
 /**
-     * The domain DTO assembler for a task.
-     */
+ * The domain DTO assembler for a task.
+ */
 
             TaskDomainDTOAssembler taskDomainDTOAssembler;
 
@@ -40,16 +41,18 @@ public class TaskService {
     @Autowired
 
 /**
-     * The Analyzer service.
-     */
+ * The Analyzer service.
+ */
 
             AnalyzerService analyzerService;
 
     @Autowired
     TaskRepository taskRepository;
+    @Autowired
+    BlackListService blackListService;
 
 
-/**
+    /**
      * Creates a new task with a NewTaskInfoDTO received by parameter.
      * Cleans up the input text by calling cleanUpInputText() and
      * analyzes it with the service analyzer.
@@ -60,18 +63,24 @@ public class TaskService {
      * @throws IOException    - thrown by IndexReader class if some sort of I/O problem occurred
      */
 
-    public TaskDTO createTask(NewTaskInfoDTO userInput) throws ParseException, IOException {
+    public Optional<TaskDTO> createTask(NewTaskInfoDTO userInput) throws ParseException, IOException {
 //        String cleanedUp = cleanUpInputText(string.getText());
 //        String language = analyzerService.analyze(cleanedUp);
-        Category category = new Category(userInput.getCategory());
-        Task task = new Task(userInput.getUrl(), userInput.getTimeOut(), category);
-        Task taskRepo = taskRepository.saveTask(task);
-        return taskDomainDTOAssembler.toDTO(taskRepo);
-    }
+        NewBlackListInfoDTO newBlackListInfoDTO = new NewBlackListInfoDTO(userInput.getUrl());
+
+        if (blackListService.isBlackListed(newBlackListInfoDTO)) {
+            return Optional.empty();
+        } else {
+            Category category = new Category(userInput.getCategory());
+            Task task = new Task(userInput.getUrl(), userInput.getTimeOut(), category);
+            Task taskRepo = taskRepository.saveTask(task);
+            return Optional.of(taskDomainDTOAssembler.toDTO(taskRepo));
+        }
 
    /* public TaskDTO findByCategory(NewTaskInfoDTO userInput) throws ParseException, IOException{
 
     }*/
 
+    }
 }
 
