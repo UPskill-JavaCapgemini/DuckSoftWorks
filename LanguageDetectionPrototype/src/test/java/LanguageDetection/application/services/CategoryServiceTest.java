332f50e109dbb3c9ee7938ba7d45647ff64854fa
Thales Lemos <thalesflemos@gmail.com>
2022-06-10 20:02:21 +0100

    added tests to CategoryService and CategoryRepository

--- LanguageDetectionPrototype/src/test/java/LanguageDetection/application/services/CategoryServiceTest.java
+++ LanguageDetectionPrototype/src/test/java/LanguageDetection/application/services/CategoryServiceTest.java
@@ -1,78 +1,101 @@
 package LanguageDetection.application.services;
+
 import LanguageDetection.application.DTO.CategoryDTO;
 import LanguageDetection.application.DTO.DTOAssemblers.CategoryDomainDTOAssembler;
 import LanguageDetection.application.DTO.NewCategoryInfoDTO;
 import LanguageDetection.domain.entities.Category;
 import LanguageDetection.domain.entities.ICategory;
-import LanguageDetection.infrastructure.repositories.CategoryRepository;
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
+import java.net.MalformedURLException;
+import java.util.List;
 import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-
 
 
 class CategoryServiceTest {
-//
-//    @Mock
-//    CategoryDomainDTOAssembler dtoAssembler;
-//
-//    @Mock
-//    ICategory iCategory;
-//
-//    @InjectMocks
-//    CategoryService categoryService;
-//
-//
-//    @BeforeEach
-//    public void init() {
-//        MockitoAnnotations.openMocks(this);
-//    }
-//
-//
+
+    @Mock
+    private ICategory iCategory;
+
+    @Mock
+    CategoryDomainDTOAssembler assembler;
+
+
+    @InjectMocks
+    private CategoryService categoryService;
+
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+
+    @Test
+    void shouldCreateAndSaveCategory(){
+        String catName = "History";
+        Category item1 = new Category(catName);
+
+        Mockito.when(iCategory.findCategoryById(item1)).thenReturn(Mockito.any());
+        Mockito.when(iCategory.saveCategory(item1)).thenReturn(item1);
+        Mockito.when(assembler.toDTO(item1)).thenReturn(new CategoryDTO(item1));
+
+        Optional<CategoryDTO> optional =  categoryService.createAndSaveCategory(new NewCategoryInfoDTO(catName));
+        Assert.assertTrue(!optional.isEmpty());
+        Assert.assertTrue(optional.get().getCategory().toString().equals(catName));
+    }
+
 //    @Test
-//    void shouldCreateACategoryWithCorrectAttributes() {
+//    void shouldSuccessefullyDeleteACategory() {
+//        String catName = "History";
+//        Category item1 = new Category(catName);
+//        NewCategoryInfoDTO newCategoryInfoDTOitem1 = new NewCategoryInfoDTO(catName);
 //
-//        NewCategoryInfoDTO specificDTO = new NewCategoryInfoDTO("Economics");
-//        Category category = new Category("Economics");
-//        CategoryDTO categoryDTO = new CategoryDTO(category);
+//        Mockito.when(iCategory.deleteByName(item1)).thenReturn(true);
 //
-//        // Arrange
-//        when(iCategory.saveCategory(category)).thenReturn(category);
-//        when(dtoAssembler.toDTO(category)).thenReturn(categoryDTO);
-//
-//        // Act
-//        Optional<CategoryDTO> categoryReturn = categoryService.createAndSaveCategory(specificDTO);
-//
-//        String catName = categoryReturn.toString();
-//
-//        // Assert
-//        assertEquals(catName, "Economics");
+//        Assert.assertTrue(categoryService.deleteCategory(newCategoryInfoDTOitem1));
 //    }
-//
-//
 
+    @Test
+    void getAllCategoryItemsWith2CreatedAssertingTrue() throws MalformedURLException {
+        Category item1 = new Category("Arts");
+        Category item2 = new Category("Science");
 
-   /* @Test
-    void findAll() {
+        Mockito.when(iCategory.findAll()).thenReturn(List.of(item1,item2));
+        Mockito.when(assembler.toDTO(item1)).thenReturn(new CategoryDTO(item1));
+        Mockito.when(assembler.toDTO(item2)).thenReturn(new CategoryDTO(item2));
+
+        List<CategoryDTO> result = categoryService.getAllCategory();
+
+        Assert.assertEquals(result.size(),2);
     }
 
     @Test
-    void deleteCategory() {
+    void getAllCategoryItemsWith1CreatedAssertingFalse() throws MalformedURLException {
+        Category item1 = new Category("Arts");
+
+        Mockito.when(iCategory.findAll()).thenReturn(List.of(item1));
+        Mockito.when(assembler.toDTO(item1)).thenReturn(new CategoryDTO(item1));
+
+        List<CategoryDTO> result = categoryService.getAllCategory();
+
+        Assert.assertNotEquals(result.size(),2);
     }
 
     @Test
-    void findById() {
-    }*/
+    void categoryExistsfindByCategoryId(){
+        Category item1 = new Category("History");
+        Optional<Category> op = Optional.of(item1);
+
+        Mockito.when(iCategory.findCategoryById(Mockito.any())).thenReturn(op);
+
+        Assert.assertSame(categoryService.findById(item1), op);
+
+    }
+
 }
