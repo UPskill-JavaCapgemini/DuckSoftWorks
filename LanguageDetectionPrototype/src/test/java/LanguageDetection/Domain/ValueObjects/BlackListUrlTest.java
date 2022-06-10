00c8de8f5415b2af7132d7025d19dfd619d2363f
Inês Clavel <inesclavel@gmail.com>
2022-06-10 23:22:34 +0100

    BlackListUrlTest class created with tests

--- LanguageDetectionPrototype/src/test/java/LanguageDetection/Domain/ValueObjects/BlackListUrlTest.java
+++ LanguageDetectionPrototype/src/test/java/LanguageDetection/Domain/ValueObjects/BlackListUrlTest.java
@@ -1,7 +1,9 @@
 package LanguageDetection.Domain.ValueObjects;
 
 import LanguageDetection.domain.ValueObjects.BlackListUrl;
+import LanguageDetection.domain.ValueObjects.InputUrl;
 import LanguageDetection.domain.entities.Category;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import java.net.MalformedURLException;
@@ -17,17 +19,41 @@ class BlackListUrlTest {
         assertEquals(url.getBlackListUrlObject().toString(), "https://stackoverflow.com/first");
     }
 
-   /* @Test
-    void shouldNotCreateABlackListUrlWithIncorrectAttributes() throws MalformedURLException {
-        BlackListUrl url = new BlackListUrl("ht://stackoverflow.com/first");
+    @Test
+    void shouldNotCreateABlackListUrlWithIncorrectAttributes()  {
+        String noProtocolBlackListUrl = "ht//noprotocol.com";
 
-        Exception exception = assertThrows(MalformedURLException.class, () -> {
-            BlackListUrl url1 = new BlackListUrl("");
+        MalformedURLException malformedURLException = assertThrows(MalformedURLException.class, () -> {
+            new BlackListUrl(noProtocolBlackListUrl);
         });
 
-        assertTrue(exception.getMessage().contains("unknown protocol: "));
-    }*/
+        Assert.assertEquals(malformedURLException.getMessage(), "no protocol: ht//noprotocol.com");
+    }
+
+    @Test
+    void shouldThrowExceptionAndNotCreateABlackListUrlWithBlankUrl()  {
+        String noProtocolBlackListUrl = "";
+
+        MalformedURLException malformedURLException = assertThrows(MalformedURLException.class, () -> {
+            new BlackListUrl(noProtocolBlackListUrl);
+        });
+
+        Assert.assertEquals(malformedURLException.getMessage(), "no protocol: ");
+    }
 
 
+    @Test
+    public void shouldThrowExceptionAndNotCreateAABlackListUrlWithSpacesOnlyUrl() {
+        //Arrange
+        String spacesOnlyBlackListUrl = "             ";
+
+        //Act
+        MalformedURLException malformedURLException = assertThrows(MalformedURLException.class, () -> {
+            new BlackListUrl(spacesOnlyBlackListUrl);
+        });
+
+        //Assert
+        Assert.assertEquals(malformedURLException.getMessage(), "no protocol:              ");
+    }
 
 }
