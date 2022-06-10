85408b88f3277197b81f1c815d9006f4c3d6824f
Thales Lemos <thalesflemos@gmail.com>
2022-06-10 12:06:09 +0100

    added test shouldCreateACategoryWithCorrectAttributes

--- LanguageDetectionPrototype/src/test/java/LanguageDetection/application/services/CategoryServiceTest.java
+++ LanguageDetectionPrototype/src/test/java/LanguageDetection/application/services/CategoryServiceTest.java
@@ -1,49 +1,56 @@
-//package LanguageDetection.application.services;
-//
-//import LanguageDetection.domain.entities.Category;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class CategoryServiceTest {
-//
+package LanguageDetection.application.services;
+import LanguageDetection.domain.entities.Category;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryServiceTest {
+
+
+    @Test
+    void shouldCreateACategoryWithCorrectAttributes() {
+        Category category = new Category("Arts");
+
+        assertEquals(category.getCategoryName().toString(), "Arts");
+    }
+
+
 //    @Test
 //    void createAndSaveCategory() {
 //        Category category = new Category("Maths");
 //
 //        assertTrue();
 //    }
-//
-//
-//    /*// Arrange
-//    when(country.getCode()).thenReturn("PT");
-//    when(country.getName()).thenReturn("Portugal");
-//
-//    when( countryFactory.createCountry("PT", "Portugal")).thenReturn(country);
-//
-//    when(countryRepository.save( country )).thenReturn(country);
-//
-//    // Act
-//    Country country = countryService.createAndSaveCountry("PT", "Portugal");
-//
-//    String code = country.getCode();
-//    String name = country.getName();
-//
-//    // Assert
-//    assertEquals(code, "PT");
-//    assertEquals(name, "Portugal");
-//}*/
-//
-//
-//   /* @Test
-//    void findAll() {
-//    }
-//
-//    @Test
-//    void deleteCategory() {
-//    }
-//
-//    @Test
-//    void findById() {
-//    }*/
-//}
+
+
+    /*// Arrange
+    when(country.getCode()).thenReturn("PT");
+    when(country.getName()).thenReturn("Portugal");
+
+    when( countryFactory.createCountry("PT", "Portugal")).thenReturn(country);
+
+    when(countryRepository.save( country )).thenReturn(country);
+
+    // Act
+    Country country = countryService.createAndSaveCountry("PT", "Portugal");
+
+    String code = country.getCode();
+    String name = country.getName();
+
+    // Assert
+    assertEquals(code, "PT");
+    assertEquals(name, "Portugal");
+}*/
+
+
+   /* @Test
+    void findAll() {
+    }
+
+    @Test
+    void deleteCategory() {
+    }
+
+    @Test
+    void findById() {
+    }*/
+}
