ca3b20784444c658c49b5868bcd5e3a69fd36261
Thales Lemos <thalesflemos@gmail.com>
2022-06-19 23:22:34 +0100

    created method to add categories from DB to dropDown menu in create a category frontEnd

--- Frontend/createTask.js
+++ Frontend/createTask.js
@@ -1,25 +1,69 @@
-function MyFunction(e) {
+//populate categories list everytime load of the screen occurs 
+window.onload = populateCategories;
+
+
+// function to create a task
+function createNewTask(e) {
     e.preventDefault();
     console.log("hi there");
     var link = document.getElementById("link").value
-    var category = document.getElementById("category")
+    var category = document.getElementById("category-dropdown")
     var categoryOption = category.options[category.selectedIndex].value
     var timeLimit = document.getElementById("timeLimit")
     var timeLimitOption = timeLimit.options[timeLimit.selectedIndex].value
-  //  document.getElementById("demo").innerHTML = link + " " + category + " " + timeLimit;
-    
+
   var stringified = JSON.stringify({ url : link, category : categoryOption, timeOut : timeLimitOption })
   console.log(stringified);
   fetch('http://localhost:8080/LanguageDetection', {
       method: 'POST',
-     /* mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'include', // include, *same-origin, omit
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer',*/
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ url : link, category : categoryOption, timeOut: timeLimitOption })
-    }).then(res => console.log(res))
-  }
+    }).then(resp => {
+        var responseContent = document.getElementById("create-task-response");
+    if (resp.status === 201) {
+        responseContent.textContent = "The task was successfully created!";
+        console.log("Task was successfully created!")
+    } else {
+        responseContent.textContent = "The task couldn't be created!";
+        console.log("Task was successfully created!")
+    }
+})
+}
+
+
+// method to populate drop down menu
+function populateCategories(){
+
+let dropdown = document.getElementById('category-dropdown');
+dropdown.length = 0;
+
+const categoriesURL = 'http://localhost:8080/Category';
+
+fetch(categoriesURL)  
+  .then(  
+    function(response) {  
+      if (response.status !== 200) {  
+        console.warn('Looks like there was a problem. Status Code: ' + 
+          response.status);  
+        return;  
+      }
+
+      // Examine the text in the response  
+      response.json().then(function(data) {  
+        let option;
+    
+    	for (let i = 0; i < data.length; i++) {
+          option = document.createElement('option');
+      	  option.text = data[i].category;
+      	  option.value = data[i].category;
+      	  dropdown.add(option);
+    	}    
+      });  
+    }  
+  )  
+  .catch(function(err) {  
+    console.error('Fetch Error -', err);  
+  });
+}
--- src/main/java/LanguageDetection/application/controllers/CategoryController.java
+++ src/main/java/LanguageDetection/application/controllers/CategoryController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 
 @Controller
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(path = "/Category")
 public class CategoryController {
