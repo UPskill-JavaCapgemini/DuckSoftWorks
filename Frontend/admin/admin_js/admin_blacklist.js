1041580ef0842eadd416c78495232d178fe72542
Thales Lemos <thalesflemos@gmail.com>
2022-06-21 23:18:52 +0100

    frontend - added method to allow blacklist add and blacklist to be shown in table when returned

---
+++ Frontend/admin/admin_js/admin_blacklist.js
@@ -0,0 +1,66 @@
+window.onload = findAllBlackListItems;
+
+function createNewBlackListItem(e) {
+    e.preventDefault();
+    console.log("createBlackListInitiated");
+    var blackListInput = document.getElementById("BlackListItemInput").value
+
+  var stringified = JSON.stringify({ url : blackListInput})
+  console.log(stringified);
+  fetch('http://localhost:8080/BlackList', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ url : blackListInput}),
+      credentials:"include"
+    }).then(resp => {
+        var responseContent = document.getElementById("create-blacklist-response");
+    if (resp.status === 201) {
+        responseContent.textContent = "The BlackList item " + blackListInput + " was successfully created!";
+        console.log("BlackList item was successfully created!")
+        findAllBlackListItems(e)
+
+    } else {
+        responseContent.textContent = "The BlackList item couldn't be created! Invalid format or already blacklisted.";
+        console.log("BlackList not created")
+    }
+})
+}
+
+
+function findAllBlackListItems(e) {
+    e.preventDefault();
+    console.log("hi there");
+    let blackListTable = document.getElementById('html-blacklist-table');
+  fetch( 'http://localhost:8080/BlackList', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials:"include"
+      } ).then(resp => {
+
+    if (resp.status === 200) {
+        console.log("The blackList returned")
+        
+        resp.json().then(function(data) {  
+          let blackListEntry;   
+          blackListTable.innerHTML = ""
+      
+        for (let i = 0; i < data.length; i++) {
+
+            let row = blackListTable.insertRow(i);
+            let cell1 = row.insertCell(0);
+            cell1.innerHTML = data[i].url;
+
+
+        }    
+        });
+
+    } else {
+        console.log("Nope!")
+    }
+})
+}
+
