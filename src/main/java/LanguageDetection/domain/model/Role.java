8311de7113188d5cd4bdd39c3d5245ea5647afbe
DanielVMachado <danielmachado96@hotmail.com>
2022-06-21 15:20:08 +0100

    Add Role tests

--- src/main/java/LanguageDetection/domain/model/Role.java
+++ src/main/java/LanguageDetection/domain/model/Role.java
@@ -15,27 +15,14 @@ public class Role {
   @Column(length = 20)
   private ERole name;
 
-  public Role() {
-
+  protected Role() {
   }
 
   public Role(ERole name) {
     this.name = name;
   }
-
-  public Integer getId() {
-    return id;
-  }
-
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
   public ERole getName() {
     return name;
   }
 
-  public void setName(ERole name) {
-    this.name = name;
-  }
 }
