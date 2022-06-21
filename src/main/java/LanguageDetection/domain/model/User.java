e3e792ca2e603e29d895a27416074dd381520ae7
DanielVMachado <danielmachado96@hotmail.com>
2022-06-21 19:15:01 +0100

    Deleted unnecessary set methods from User class
    ORM constructor changed to protected, this way is not acessible outside domain

--- src/main/java/LanguageDetection/domain/model/User.java
+++ src/main/java/LanguageDetection/domain/model/User.java
@@ -37,7 +37,7 @@ public class User {
         inverseJoinColumns = @JoinColumn(name = "role_id"))
   private Set<Role> roles = new HashSet<>();
 
-  public User() {
+  protected User() {
   }
 
   public User(String username, String email, String password) {
@@ -50,39 +50,19 @@ public class User {
     return id;
   }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
-
   public String getUsername() {
     return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
   }
 
   public String getEmail() {
     return email;
   }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
   public String getPassword() {
     return password;
   }
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
   public Set<Role> getRoles() {
     return roles;
-  }
-
-  public void setRoles(Set<Role> roles) {
-    this.roles = roles;
   }
 }
