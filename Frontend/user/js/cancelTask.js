7f103cd3b5a424859384941a42a8bb116e2a79f6
Inês Clavel <inesclavel@gmail.com>
2022-06-23 10:44:57 +0100

    conflict management

--- Frontend/user/js/cancelTask.js
+++ Frontend/user/js/cancelTask.js
@@ -1,80 +1,38 @@
-<<<<<<< HEAD
+
 window.onload = filterTasksByProcessing;
 setInterval(filterTasksByProcessing, 3000);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+window.onload = filterTasksByProcessing;
+setInterval(filterTasksByProcessing, 3000);
+
+function cancelTask(e) {
+    e.preventDefault();
+    var cancelOption = document.getElementById("cancel_task_input").value
+   
+  var stringified = JSON.stringify({ id : cancelOption})
+  console.log(stringified);
+  fetch('http://localhost:8080/LanguageDetection/cancel', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id : cancelOption}),
+      credentials:"include"
+    }).then(resp => {
+        var responseContent = document.getElementById("cancel-tasks-response");
+    if (resp.status === 200) {
+        responseContent.textContent = "The task " + cancelOption + " was successfully canceled!";
+        console.log("task " + cancelOption + " was successfully canceled!") 
+        populateCategories();
+  
+    } else {
+        responseContent.textContent = "The task couldn't be canceled.";
+        console.log("task not canceled")
+    }
+  })
+  }
 
 function filterTasksByProcessing(e) {
     console.log("started filterTasksByProcessing");
@@ -153,7 +111,6 @@ function filterTasksByProcessing(e) {
     }
   })
 }
-=======
 
 function cancelTask(e) {
     e.preventDefault();
@@ -181,4 +138,3 @@ function cancelTask(e) {
     }
   })
   }
->>>>>>> 2fce15302c1cd9b5751feb902c73a8394697243f
