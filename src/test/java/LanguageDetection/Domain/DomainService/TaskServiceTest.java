7c5be8bb377c8a1f02e1362449a2280bb5e9046d
DanielLimaUpskill <daniel.lima-t15@upskill.pt>
2022-06-25 15:15:04 +0100

    Added test method for Task creation and save to TaskServiceTest

--- src/test/java/LanguageDetection/Domain/DomainService/TaskServiceTest.java
+++ src/test/java/LanguageDetection/Domain/DomainService/TaskServiceTest.java
@@ -9,17 +9,24 @@ import LanguageDetection.domain.model.Task;
 import LanguageDetection.domain.model.TaskFactory;
 import LanguageDetection.domain.model.ValueObjects.InputUrl;
 import LanguageDetection.domain.model.ValueObjects.TaskStatus;
+import LanguageDetection.domain.model.ValueObjects.TimeOut;
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.*;
+import org.springframework.test.util.ReflectionTestUtils;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 
 class TaskServiceTest {
 
@@ -128,4 +135,43 @@ class TaskServiceTest {
         }
     }
 
+
+//    @Test
+//    void ensureCreateAndSaveTaskReturnsCreatedTask() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, MalformedURLException, InstantiationException {
+//        //arrange
+//        String url = "https://www.w3.org/TR/PNG/iso_8859-1.txt";
+//        int inputTimeOut = 1;
+//        String inputCategory = "Sports";
+//        Category category = new Category(inputCategory);
+//        TimeOut timeOut = new TimeOut(inputTimeOut);
+//        InputUrl inputUrl = new InputUrl(url);
+//
+//
+//
+//        Method isBeingAnalyzed = TaskService.class.getDeclaredMethod("isBeingAnalyzed", String.class);
+//        isBeingAnalyzed.setAccessible(true);
+//
+//        Method initializeLanguageAnalysis = TaskService.class.getDeclaredMethod("initializeLanguageAnalysis", Task.class);
+//        initializeLanguageAnalysis.setAccessible(true);
+//
+//        Constructor<Task> taskConstructor = Task.class.getDeclaredConstructor(InputUrl.class,TimeOut.class,Category.class,Long.class);
+//        taskConstructor.setAccessible(true);
+//        Task task = taskConstructor.newInstance(inputUrl,timeOut,category,1L);
+//
+//
+//        Optional<Task> createdTaskOp = Optional.of(task);
+//
+//
+//        try(MockedStatic<UserDetailsDomainService> utils = Mockito.mockStatic(UserDetailsDomainService.class)){
+//            utils.when(()-> iTaskRepository.existsByUrlAndIsProcessing(any(InputUrl.class), any(Long.class))).thenReturn(false);
+//                Mockito.when(taskFactory.createTask(url, inputTimeOut, inputCategory)).thenReturn(createdTaskOp);
+//                Mockito.doNothing().when(initializeLanguageAnalysis.invoke(taskService,task));
+//
+//                //act
+//                Optional<Task> savedTask = taskService.createAndSaveTask(url, inputTimeOut, inputCategory);
+//
+//                //assert
+//                assertNotNull(savedTask);
+//            }
+//        }
 }
